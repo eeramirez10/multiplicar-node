c10ada821a7537017f8854c490c1119200e262d5
Erick <eeramirez@tuvansa.com.mx>
2020-01-07 13:22:30 -0600

    Commit inicial

---
+++ app.js
@@ -0,0 +1,19 @@
+const argv = require ('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar')
+
+let comando = argv._[0];
+
+if (comando == 'listar'){
+    listarTabla(argv.base, argv.limite)
+}else if (comando == 'crear'){
+    crearArchivo(argv.base, argv.limite)
+        .then(archivo => console.log(`Archivo creado: ${colors.green(archivo)}`))
+        .catch(err => console.log(err))
+}else{
+    console.log('comando no reconocido')
+}
+
+
+
